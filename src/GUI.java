09bf913170ab204dcf8c7933790b5e33e97530d1
arcane <kevindraw@comcast.net>
2021-01-28 09:54:38 -0800

    text in GUI
    figured out a basic layout for text boxes

--- src/GUI.java
+++ src/GUI.java
@@ -15,21 +15,46 @@ import javax.swing.*;
 
 public class GUI
 {
+
     public GUI()
     {
+
         //Create the frame.
         JFrame frame = new JFrame("Secure Voting App");
 
         //Exit when closed.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         //Create components and put them in the frame.
+
         JLabel instructions = new JLabel("You are about to place your vote. But first, please enter your given information here:", SwingConstants.LEFT);
-        frame.getContentPane().add(instructions, BorderLayout.CENTER);
+        instructions.setBounds(50,80,500, 40);
+
+        JLabel title = new JLabel("First Name:", SwingConstants.LEFT);
+        title.setBounds(50,140,500, 40);
+
+        JTextField text = new JTextField("",10);
+        text.setBounds(50,180,140, 40);
+
+        JLabel title2 = new JLabel("Last Name:", SwingConstants.LEFT);
+        title2.setBounds(50,240,500, 40);
+
+        JTextField text2 = new JTextField("",10);
+        text2.setBounds(50,280,140, 40);
 
 
+        frame.getContentPane().add(instructions);
+        frame.getContentPane().add(title);
+        frame.getContentPane().add(title2);
+
+        frame.getContentPane().add(text);
+        frame.getContentPane().add(text2);
+
+        frame.setSize(600, 420);
+        frame.setLayout(null);
         //Size the frame.
-        frame.pack();
+
 
         //Show it.
         frame.setVisible(true);
