fb7e4feaddab237799df6c8ee3b55786c5321918
arcane <kevindraw@comcast.net>
2021-02-05 21:07:13 -0800

    Update ClientGUI
    Added it so it can change screens.
    It keeps a list of candidates.

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -11,28 +11,47 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class ClientGUI
 {
+    public JFrame frame;
+    private ArrayList<String> candidates;
 
     public ClientGUI()
     {
 
         //Create the frame.
-        JFrame frame = new JFrame("Secure Voting App");
+        frame = new JFrame("Secure Voting App");
+
+        //Exit when closed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        candidates = new ArrayList<String>();
+        candidates.add("Inn C. Umbent");
+        candidates.add("Chel Enger");
+
+        WelcomeScreen();
+
+        frame.setLayout(null);
+        frame.setSize(715, 800);
+
+        //Size the frame.
+
+
+
+        //Show it.
+        frame.setVisible(true);
+    }
+
+    public void ExitScreen() {
+        frame.setContentPane(new JPanel(new BorderLayout()));
 
         //Exit when closed.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-
-
-
-
-
-        //After Voting Screen
-        /*  After receipt
         JLabel instructions = new JLabel("Thank you for voting!", SwingConstants.CENTER);
         instructions.setBounds(100,80,500, 40);
         instructions.setFont(new Font("Tacoma",Font.BOLD, 20));
@@ -46,69 +65,29 @@ public class ClientGUI
         instructions3.setFont(new Font("Tacoma",Font.PLAIN, 20));
         frame.getContentPane().add(instructions);
         frame.getContentPane().add(instructions2);
-        frame.getContentPane().add(instructions3);*/
-
-        //Confirmation Screen
-        /*  Confirmation Page
-        JLabel instructions = new JLabel("Please review the information before Submitting", SwingConstants.LEFT);
-        instructions.setBounds(130,80,500, 40);
-        instructions.setFont(new Font("Tacoma",Font.BOLD, 20));
-
-        JLabel votes1 = new JLabel("One listed name per vote", SwingConstants.LEFT);
-        votes1.setBounds(100,150,200, 70);
-        votes1.setFont(new Font("Tacoma",Font.PLAIN, 18));
-
-        JButton b=new JButton("Make Changes");
-        b.setBounds(100,650,180, 50);
-        b.setFont(new Font("Tacoma",Font.BOLD, 18));
-
-        JButton b2=new JButton("Confirm");
-        b2.setBounds(400,650,180, 50);
-        b2.setFont(new Font("Tacoma",Font.BOLD, 18));
-
-        frame.getContentPane().add(instructions);
-        frame.getContentPane().add(votes1);
-        frame.getContentPane().add(b);
-        frame.getContentPane().add(b2);*/
-
-        //Voting Page
-
-        JLabel instructions = new JLabel("Choose a candidate from each position", SwingConstants.LEFT);
-        instructions.setBounds(150,80,500, 40);
-        instructions.setFont(new Font("Tacoma",Font.BOLD, 20));
-
-        //repeat this section for each opening
-        JLabel section1 = new JLabel("1) Senator", SwingConstants.LEFT);
-        section1.setBounds(100,130,200, 70);
-        section1.setFont(new Font("Tacoma",Font.PLAIN, 18));
-
-        JRadioButton option1 = new JRadioButton("Inn C. Umbent");
-        JRadioButton option2 = new JRadioButton("Sal Enger");
-        option1.setBounds(100, 180, 150, 40);
-        option1.setFont(new Font("Tacoma",Font.PLAIN, 16));
-        option2.setBounds(375, 180, 150, 40);
-        option2.setFont(new Font("Tacoma",Font.PLAIN, 16));
-
-        ButtonGroup group = new ButtonGroup();
-
-        group.add(option1);
-        group.add(option2);
-
-        JButton b2=new JButton("Confirm");
-        b2.setBounds(400,650,180, 50);
-        b2.setFont(new Font("Tacoma",Font.BOLD, 18));
-
-        frame.getContentPane().add(b2);
-
-        frame.getContentPane().add(instructions);
-        frame.getContentPane().add(section1);
-
-        frame.getContentPane().add(option1);
-        frame.getContentPane().add(option2);
+        frame.getContentPane().add(instructions3);
 
 
-        //Welcome
-        /*   Start Menu
+
+        frame.setLayout(null);
+        frame.setSize(715, 800);
+
+        //Size the frame.
+
+
+        //Show it.
+        frame.setVisible(true);
+
+    }
+
+
+    public void WelcomeScreen() {
+        frame.setContentPane(new JPanel(new BorderLayout()));
+
+        //Exit when closed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         JLabel instructions = new JLabel("Welcome! Please Select Your Language", SwingConstants.LEFT);
         instructions.setBounds(130,80,500, 40);
         instructions.setFont(new Font("Tacoma",Font.BOLD, 24));
@@ -140,9 +119,9 @@ public class ClientGUI
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                //next screen
+                VoteScreen("");
             }
-        });*/
+        });
 
 
 
@@ -153,6 +132,146 @@ public class ClientGUI
 
 
         //Show it.
+        frame.setVisible(true);
+
+    }
+
+
+    public void ConfirmScreen(String selected) {
+
+        frame.setContentPane(new JPanel(new BorderLayout()));
+
+        //Exit when closed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel instructions = new JLabel("Please review the information before Submitting", SwingConstants.LEFT);
+        instructions.setBounds(130,80,500, 40);
+        instructions.setFont(new Font("Tacoma",Font.BOLD, 20));
+
+        JLabel section1 = new JLabel("1) Senator", SwingConstants.LEFT);
+        section1.setBounds(150,130,200, 70);
+        section1.setFont(new Font("Tacoma",Font.PLAIN, 18));
+
+
+        JLabel candidate1 = new JLabel( "No Candidate Selected", SwingConstants.LEFT);
+        if(!selected.equals("")){
+
+            candidate1.setText(candidates.get(Integer.parseInt(selected) - 1));
+        }
+        candidate1.setBounds(350,130,200, 70);
+        candidate1.setFont(new Font("Tacoma",Font.PLAIN, 18));
+
+
+        JButton b=new JButton("Make Changes");
+        b.setBounds(100,650,180, 50);
+        b.setFont(new Font("Tacoma",Font.BOLD, 18));
+
+        JButton b2=new JButton("Confirm");
+        b2.setBounds(400,650,180, 50);
+        b2.setFont(new Font("Tacoma",Font.BOLD, 18));
+
+        frame.getContentPane().add(instructions);
+        frame.getContentPane().add(section1);
+        frame.getContentPane().add(candidate1);
+        frame.getContentPane().add(b);
+        frame.getContentPane().add(b2);
+
+        frame.setLayout(null);
+        frame.setSize(715, 800);
+
+        //Size the frame.
+        b.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                VoteScreen(selected);
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                ExitScreen();
+            }
+        });
+
+        //Show it.
+        frame.setVisible(true);
+
+    }
+
+
+    public void VoteScreen(String selected){
+        //frame.removeAll();
+        frame.setContentPane(new JPanel(new BorderLayout()));
+
+        //Exit when closed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //frame.removeAll();
+
+        JLabel instructions = new JLabel("Choose a candidate from each position", SwingConstants.LEFT);
+        instructions.setBounds(150,80,500, 40);
+        instructions.setFont(new Font("Tacoma",Font.BOLD, 20));
+
+        //repeat this section for each opening
+        JLabel section1 = new JLabel("1) Senator", SwingConstants.LEFT);
+        section1.setBounds(100,130,200, 70);
+        section1.setFont(new Font("Tacoma",Font.PLAIN, 18));
+
+        JRadioButton option1 = new JRadioButton(candidates.get(0));
+        JRadioButton option2 = new JRadioButton(candidates.get(1));
+        option1.setBounds(100, 180, 150, 40);
+        option1.setFont(new Font("Tacoma",Font.PLAIN, 16));
+        option2.setBounds(375, 180, 150, 40);
+        option2.setFont(new Font("Tacoma",Font.PLAIN, 16));
+
+        ButtonGroup group = new ButtonGroup();
+
+        group.add(option1);
+        group.add(option2);
+
+        if(!selected.equals("")) {
+
+
+            if(Integer.parseInt(selected)== 1){
+                option1.doClick();
+            }else if(Integer.parseInt(selected)== 2){
+                option2.doClick();
+            }
+        }
+
+        JButton b2=new JButton("Confirm");
+        b2.setBounds(400,650,180, 50);
+        b2.setFont(new Font("Tacoma",Font.BOLD, 18));
+
+        frame.getContentPane().add(b2);
+
+        frame.getContentPane().add(instructions);
+        frame.getContentPane().add(section1);
+
+        frame.getContentPane().add(option1);
+        frame.getContentPane().add(option2);
+
+        b2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                String send = "";
+                if(option1.isSelected()){
+                    send = "1";
+                }else if(option2.isSelected()){
+                    send = "2";
+                }
+                ConfirmScreen(send);
+            }
+        });
+
+        frame.setLayout(null);
+        frame.setSize(715, 800);
+
+
         frame.setVisible(true);
     }
 }
